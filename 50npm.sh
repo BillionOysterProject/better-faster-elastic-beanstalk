d3a5abddeaf49e9e4c10c1b2e70ab623795c8c97
deld <delalid@fearsol.com>
2016-02-17 00:11:48 -0500

    test
    test

--- 50npm.sh
+++ 50npm.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
-. /opt/elasticbeanstalk/env.vars
-function error_exit
-{
-  eventHelper.py --msg "$1" --severity ERROR
-  exit $2
-}
-
-#install not-installed yet app node_modules
-if [ ! -d "/var/node_modules" ]; then
-  mkdir /var/node_modules ;
-fi
-if [ -d /tmp/deployment/application ]; then
-  ln -s /var/node_modules /tmp/deployment/application/
-fi
-
-OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v4.2.3-linux-x64/bin/npm install 2>&1) || error_exit "Failed to run npm install.  $OUT" $?
-echo $OUT
+ chmod -R o+r /var/node_modules
