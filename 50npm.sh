8a6408208609f2a1b0e16bce45a48303c17898fc
deld <delalid@fearsol.com>
2016-02-17 21:41:44 -0500

    added bower install

--- 50npm.sh
+++ 50npm.sh
@@ -49,7 +49,13 @@ if [ -d /tmp/deployment/application ]; then
 fi
 
 echo "------------------------------ — Installing/updating NPM modules, it might take a while, go take a leak or have a healthy snack... — -----------------------------------"
-OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm install) || error_exit "Failed to run npm install.  $OUT" $?
+OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm install) || error_exit "*Failed to run npm install.  $OUT" $?
+echo $OUT
+
+echo "------------------------------ — Running Bower install -----------------------------------"
+OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/lib/node_modules/bower/bin/bower install) || error_exit "**Failed to run bower install.  $OUT" $?
 echo $OUT
 
 chmod -R o+r /var/node_modules
+chmod -R o+r /tmp/deployment/application
+
