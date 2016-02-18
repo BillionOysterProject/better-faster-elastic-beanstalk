15acdb99feb8eddce73ed47d94fedc5d483d5962
deld <delalid@fearsol.com>
2016-02-17 22:15:21 -0500

    removing bower install

--- 50npm.sh
+++ 50npm.sh
@@ -53,10 +53,6 @@ echo "------------------------------ — Installing/updating NPM modules, it mig
 OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm install) || error_exit "*Failed to run npm install.  $OUT" $?
 echo $OUT
 
-echo "------------------------------ — Running Bower install -----------------------------------"
-OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/lib/node_modules/bower/bin/bower install) || error_exit "**Failed to run bower install.  $OUT" $?
-echo $OUT
-
 chmod -R o+r /var/node_modules
 chmod -R o+r /tmp/deployment/application
 
